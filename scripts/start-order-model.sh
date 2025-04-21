06511143f795bf9297633b009c1c6f14dc70bcce
DG-git-dev <don_research@163.com>
2025-04-21 13:27:46 +0800

    Fix typo error of "order_moder"

--- scripts/start-order-model.sh
+++ scripts/start-order-model.sh
@@ -4,7 +4,7 @@
 ray stop
 
 # build and start the model serving app
-# serve build market_simulation.rollout.order_moder_serving:order_model_app -o ray_serving.yaml
+# serve build market_simulation.rollout.order_model_serving:order_model_app -o ray_serving.yaml
 ray start --head
 serve deploy ray_serving.yaml
 serve status
