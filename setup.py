aa125750f63436ee65bb498cecfa473b2c11766b
DG-git-dev <don_research@163.com>
2026-09-24 11:45:48 +0800

    Set MarS version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="market_simulation",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(),
     description="An awesome package that does something",
     install_requires=[
